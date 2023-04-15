dbb10821268949016b51230ee350ef471856b6dd
ShazuxD <116494974+ShazuxD@users.noreply.github.com>
2023-04-15 21:50:26 +0530

    Update upload.sh

--- CI_scripts/upload.sh
+++ CI_scripts/upload.sh
@@ -1,3 +1,13 @@
 cd /tmp/ci/out/target/product/RMX1801
 ls
 curl bashupload.com -T P*.zip
+ls
+sudo apt install pigz
+cd /tmp
+com () 
+{ 
+    tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
+}
+
+time com ccache 1
+curl bashupload.com -T ccache*.gz
